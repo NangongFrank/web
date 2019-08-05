4d77081663734082aba71a16fcaf116ab9c7b5b6
zhao mac <3345@qq.com>
2019-08-05 08:59:32 +0800

    update String.replace

--- js/RegExp.js
+++ js/RegExp.js
@@ -1,10 +1,25 @@
-let reg = /!(:\/{2,5})/g,
-	http = 'https://www.ngfrankgl.cn//age'
+// let reg = /!(:\/{2,5})/g,
+// 	http = 'https://www.ngfrankgl.cn//age'
 
-let fetch = http.match(reg),
-	test = reg.test(http)
-console.log(fetch)
-// console.log(test)
-// 
-let str = 'name'
-decodeURIComponent()
+// let fetch = http.match(reg),
+// 	test = reg.test(http)
+// console.log(fetch)
+// // console.log(test)
+// // 
+// let str = 'name'
+// decodeURIComponent()
+
+/*  String.replace()  */
+let s = 'javascript'
+
+let b = s.replace(/(java)(script)/, "$2$1")
+// console.log(b)
+/**
+ * $1, $2, ..., $99 与正则表达式中的1-99个子表达式相匹配的文本
+ * $& 与正则表达式相匹配的字符串
+ * $` 位于匹配子字符的匹配字符串
+ * $' 位于匹配子字符串右侧的文本
+ * $$ 表示$符号
+ */
+let c = s.replace(/ja.+/, "$&$&")
+console.log(c)
--- js/String.js
+++ js/String.js
@@ -14,3 +14,40 @@
 /**
  * repeat()
  */
+
+/* 获取字符串的字节长度需要通过手工计算 - 处理方式有问题bug */ 
+// 方法一
+/*String.prototype.lengthB = () => {
+	let b = 0,
+		l = this.length
+	if(l) {
+		for(let i = 0; i < l; ++ i) {
+			if(this.charCodeAt(i) > 255) b +=2 
+			else ++ b
+		}
+		return b
+	} else {
+		return 0
+	}
+}
+let s = 'String对象长度'
+console.log(s.lengthB())
+
+// 方法二
+function len() {
+	let c = ''
+	for(let i = 0; i < len; ++i) {
+		c = this.charAt(i)
+		if(escape(c).length > 4) b += 2
+		else if(c != '\r') ++ b
+	}
+}
+// 正则方法
+function len2() {
+	let c = ''
+	for(let i = 0; i < l; ++ i) {
+		c = this.charAt(i)
+		if(/^[\u0000-\u00ff]$/.test(c)) ++b
+		else b += 2 
+	}
+}*/
