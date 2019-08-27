df71b61c63440573c128bd89f3c0119b9323b4f9
zhao mac <3345@qq.com>
2019-08-27 09:54:20 +0800

    function generator Object

--- js/function.js
+++ js/function.js
@@ -3,11 +3,20 @@
 // console.log(fn().name)
 
 // `*`函数 - js函数生成器
-function* fn() {
-	console.log(1)
-	yield
-	console.log(2)
+// function* fn() {
+// 	console.log(1)
+// 	yield
+// 	console.log(2)
+// }
+// let f = fn()
+// f.next()
+// f.next()
+
+function* add(x) {
+	let y = yield x + 2
+	return y
 }
-let f = fn()
-f.next()
-f.next()
+let obj = add(1)
+console.log(obj.next())
+console.log(obj.next())
+console.log(obj.next())
