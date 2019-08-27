257811437bee21436fa5be0d67836a33e7f00ec4
zhao mac <3345@qq.com>
2019-08-27 09:48:18 +0800

    function generator

--- js/function.js
+++ js/function.js
@@ -1,3 +1,13 @@
 // 箭头函数返回参数
 // let fn = e => ({name: 'zhang'})
 // console.log(fn().name)
+
+// `*`函数 - js函数生成器
+function* fn() {
+	console.log(1)
+	yield
+	console.log(2)
+}
+let f = fn()
+f.next()
+f.next()
