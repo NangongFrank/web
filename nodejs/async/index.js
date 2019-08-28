51c0d877f0ca0570953f7f9d2291d144eb174a31
zhao mac <3345@qq.com>
2019-08-28 09:52:06 +0800

    update async, catch Error

--- nodejs/async/index.js
+++ nodejs/async/index.js
@@ -2,7 +2,7 @@
 * @Author: frank_zhao
 * @Date:   2019-08-28 09:36:18
 * @Last Modified by:   zhao mac
-* @Last Modified time: 2019-08-28 09:44:26
+* @Last Modified time: 2019-08-28 09:51:26
 */
 let fs = require('fs'),
 	path = require('path'),
@@ -17,4 +17,21 @@ async function file(filepath) {  // await 后跟随的方法必须是一个`Prom
 	let buf = Buffer.concat([buf1,buf2], len)
 	console.log(buf.toString())
 }
-file()
+// file()
+async function testFile(filepath) {  // 使用`try...catch`捕获异常
+	try{
+		let buf1 = await readFile(path.join(__dirname, 'file', '1log1'))
+		let buf2 = await readFile(path.join(__dirname, 'file', 'log2'))
+		let len = buf1.length + buf2.length
+		let buf = Buffer.concat([buf1,buf2], len)
+		return buf
+	} catch(err) {
+		throw err
+	}
+}
+
+testFile().then(buf => {
+	console.log(buf.toString())
+}).catch(err => {
+	console.log(err, 'err')
+})
